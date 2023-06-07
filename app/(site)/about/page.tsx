a1df3368a186ee91dde1281028b13afa4051917f
eyadal <31968847+eyadal@users.noreply.github.com>
2023-06-08 00:01:26 +0200

    create about, contact, and projects page

---
+++ app/(site)/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>About Page</h1>
+    </main>
+  )
+}
+export default page
---
+++ app/(site)/projects/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1> Project Page</h1>
+    </main>
+  )
+}
+
+export default page
