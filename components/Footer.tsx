14e44d057fc18ace8a15940bfe10fcbe8f07a615
eyadal <31968847+eyadal@users.noreply.github.com>
2023-06-07 23:59:41 +0200

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <small>My Footer</small>
+    </footer>
+  )
+}
+
+export default Footer
