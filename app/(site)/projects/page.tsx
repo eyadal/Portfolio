43ff044320d4cc382997942571d30048baabd74e
eyadal <31968847+eyadal@users.noreply.github.com>
2023-06-08 02:58:07 +0200

    Fetch Repositories, render them on screen

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Suspense } from 'react'
 
 type Props = {}
 
-const page = (props: Props) => {
+interface Repository {
+  id: number
+  name: string
+  html_url: string
+}
+
+const page = async (props: Props) => {
+  async function getData() {
+    const res = await fetch('https://api.github.com/users/eyadal/repos')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+  }
+  const data = await getData()
+  const myRepos = data.map(({ id, name, html_url }: Repository) => (
+    <li key={id}>
+      <a href={html_url}>{name}</a>
+    </li>
+  ))
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1> Project Page</h1>
+    <main className='flex flex-col items-center justify-between '>
+      <h1 className='text-4xl py-4'>Projects</h1>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>{myRepos}</ul>
+      </Suspense>
     </main>
   )
 }
